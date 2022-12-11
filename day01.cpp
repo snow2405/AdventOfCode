f07b16d0c78724a3b2cd21be1541e0c005a206a2
Lion Six <hey@lionsix.ch>
2022-12-11 16:08:09 +0100

    upload

---
+++ day01.cpp
@@ -0,0 +1,37 @@
+// Copyright Lion Six 2022
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+
+    ifstream data;
+    data.open("data.txt");
+
+    int currentCount = 0;
+    vector<int> elfCalories;
+
+    for (string line; getline(data, line);)
+    {
+        if (line == "")
+        {
+            elfCalories.push_back(currentCount);
+            currentCount = 0;
+        }
+        else
+        {
+            currentCount += stoi(line);
+        }
+    }
+
+    sort(elfCalories.begin(), elfCalories.end());
+
+    cout << elfCalories[elfCalories.size() - 1] + elfCalories[elfCalories.size() - 2] + elfCalories[elfCalories.size() - 3] << endl;
+    return 0;
+}
